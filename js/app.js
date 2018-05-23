e820039256442e27eae22be56c6aae2bd8d3b1eb
Matas <matas.gutauskas@gmail.com>
2018-05-23 19:54:28 +0300

    List of cards, shuffling, inserting into the page JS

---
+++ js/app.js
@@ -0,0 +1,42 @@
+// List that holds all the cards
+
+const cards = document.querySelectorAll(".card");
+var cardList = [];
+
+for (var i = 0; i < cards.length; i++) {
+	cardList.push(cards[i]);
+}
+
+
+// Shuffle function from http://stackoverflow.com/a/2450976.
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+// Looping through each card and inserting shuffled cards in to the HTML
+
+function cardHTML() {
+	const shuffledCards = shuffle(cardList);
+	var dec = document.querySelector('.board');
+	for (var i = 0; i < shuffledCards.length; i++) {
+		dec.appendChild(shuffledCards[i]);
+}
+}
+
+// Setting up event listiner for the cards and creating a function that will uncover the card
+
+function cardFlip() {
+	
+}
+
+
